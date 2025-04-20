3dfa5ae3ea8ecccbabf28914d34957db2bf68822
MING <54872601+1998code@users.noreply.github.com>
2025-04-20 13:06:13 +0800

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftGlass",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SwiftGlass",
+            targets: ["SwiftGlass"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SwiftGlass"),
+        .testTarget(
+            name: "SwiftGlassTests",
+            dependencies: ["SwiftGlass"]
+        ),
+    ]
+)
